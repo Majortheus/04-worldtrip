576eecadb474718a4741b967461ae66dfbc1f991
Matheus Limão Brites <majortheus@gmail.com>
2021-09-03 13:24:35 -0300

    added categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
         <Image src="/Logo.svg" width="187" height="46" alt="worldtrip"/>
       </header>
       
-      <div className={styles.heroBanner}>
+      <div className={styles.heroBannerImage}></div>
+      <div className={styles.banner}>
         <h1>
           5 Continentes,<br/>
           infinitas possibilidades.
@@ -16,10 +17,42 @@ export default function Home() {
         <p>	
           Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
         </p>
+        <div className={styles.airplane}>
+          <Image src="/Airplane.png" width="417" height="271" alt="Avião" />
+        </div>
       </div>
       
-      <div>
-        
+      <div className={styles.categories}>
+        <div>
+          <Image src="/icons/cocktail.svg" width="85" height="85" alt="coquetel" />
+          <h4>
+            vida noturna
+          </h4>
+        </div>
+        <div>
+          <Image src="/icons/surf.svg" width="85" height="85" alt="praia" />
+          <h4>
+            praia
+          </h4>
+        </div>
+        <div>
+          <Image src="/icons/building.svg" width="85" height="85" alt="prédio" />
+          <h4>
+            moderno
+          </h4>
+        </div>
+        <div>
+          <Image src="/icons/museum.svg" width="85" height="85" alt="museu" />
+          <h4>
+            clássico
+          </h4>
+        </div>
+        <div>
+          <Image src="/icons/earth.svg" width="85" height="85" alt="terra" />
+          <h4>
+            e mais...
+          </h4>
+        </div>
       </div>
       <div>
         
