1ce56637f588d9aeed44fedb23129f588b8cbbd4
Matheus Limão Brites <majortheus@gmail.com>
2021-09-08 13:31:29 -0300

    migrated to use chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme  } from "@chakra-ui/react"
+
+const colors = {
+  brand: {
+    900: "#FFBA0880",
+    500: "#FFBA08",
+  },
+}
+
+const fonts = {
+  body: "Poppins, system-ui, sans-serif",
+  heading: "Poppins, system-ui, sans-serif",
+  mono: "Menlo, monospace",
+}
+
+const theme = extendTheme({ colors, fonts })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,73 @@
+import { Box, Flex, Heading, Stack, Text, Image as ChakraImage } from '@chakra-ui/react'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <header>
+    <Flex flex={1} direction="column" w="100%" margin="0 auto" maxW={1120}>
+      <Flex as="header" flex={1} align="center" justify="center" p="27px 0px">
         <Image src="/Logo.svg" width="187" height="46" alt="worldtrip"/>
-      </header>
+      </Flex>
       
-      <div className={styles.heroBannerImage}></div>
-      <div className={styles.banner}>
-        <h1>
+      <ChakraImage 
+        h={335} 
+        pos="absolute" 
+        w="100vw" 
+        top={100} 
+        left={0} 
+        src="/Banner.png" 
+        objectFit="cover" />
+      <Flex direction="column" justify="center" pos="relative" h={370}>
+        <Heading color="white">
           5 Continentes,<br/>
           infinitas possibilidades.
-        </h1>
-        <p>	
+        </Heading>
+        <Text color="white">	
           Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
-        </p>
-        <div className={styles.airplane}>
+        </Text>
+        <Box pos="absolute" rotateX="-3deg" right={0} bottom={0}>
           <Image src="/Airplane.png" width="417" height="271" alt="Avião" />
-        </div>
-      </div>
+        </Box>
+      </Flex>
       
-      <div className={styles.categories}>
-        <div>
+      <Flex marginTop={25}>
+        <Stack spacing={5} flex={1} align="center">
           <Image src="/icons/cocktail.svg" width="85" height="85" alt="coquetel" />
-          <h4>
+          <Text as="h4" fontSize="lg">
             vida noturna
-          </h4>
-        </div>
-        <div>
+          </Text>
+        </Stack>
+        <Stack spacing={5} flex={1} align="center">
           <Image src="/icons/surf.svg" width="85" height="85" alt="praia" />
-          <h4>
+          <Text as="h4" fontSize="lg">
             praia
-          </h4>
-        </div>
-        <div>
+          </Text>
+        </Stack>
+        <Stack spacing={5} flex={1} align="center">
           <Image src="/icons/building.svg" width="85" height="85" alt="prédio" />
-          <h4>
+          <Text as="h4" fontSize="lg">
             moderno
-          </h4>
-        </div>
-        <div>
-          <Image src="/icons/museum.svg" width="85" height="85" alt="museu" />
-          <h4>
+          </Text>
+        </Stack>
+        <Stack spacing={5} flex={1} align="center">
+        <Image src="/icons/museum.svg" width="85" height="85" alt="museu" />
+          <Text as="h4" fontSize="lg">
             clássico
-          </h4>
-        </div>
-        <div>
+          </Text>
+        </Stack>
+        
+        <Stack spacing={5} flex={1} align="center">
           <Image src="/icons/earth.svg" width="85" height="85" alt="terra" />
-          <h4>
-            e mais...
-          </h4>
-        </div>
-      </div>
+          <Text as="h4" fontSize="lg">
+          e mais...
+          </Text>
+        </Stack>
+      </Flex>
       <div>
         
       </div>
       <div>
         
       </div>
-    </div>
+    </Flex>
   )
 }
